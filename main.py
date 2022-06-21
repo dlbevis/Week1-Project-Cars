5b141a70054f0a7a67cdde1534204c9148be0977
dlbevis <107512255+dlbevis@users.noreply.github.com>
2022-06-21 00:28:28 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,77 @@
+# possibleVariables
+
+
+# Make header
+print("==========")
+print("Welcome to UMBC")
+print("Car Customization Form!")
+print("==========")
+
+# add a blank line for pretty stuff
+print()
+
+#Tell them what to do
+
+print("(For multiple Choice Problems, please enter the letter of the selection you're looking for)")
+# Make questions
+# What make of car
+variableFord = "Ford"
+print()
+
+# What model of car Ford
+# Must be multiple choice
+
+messageOne = "1. What Model of Ford would you like? \na. F150 \nb. F250 \nc. F350 \nd. F450"
+print(messageOne)
+answer1 = input("Please enter 'a' - 'd' = ")
+print(answer1)
+
+print()
+
+# question 2
+# Must be yes/No
+print("~Interior~")
+
+print("2. Would you like a USB Hub? ")
+answer2 = input(f"Enter 'yes' or 'no': ")
+print(answer2)
+
+print() 
+
+# What goes on the outside of the car, questions
+
+print("~Exterior~")
+messageThree ="3. You may choose a color: \na. blue \nb. green \nc. red"
+print(messageThree)
+answer3 = input("Please enter 'a' - 'c' = ")
+print(answer3)
+
+print() 
+
+messagefour = print("4. Would you like running boards? ")
+answer4 = input("Enter 'yes'or 'no': ")
+print(answer4)
+
+print()              
+#enter a blank line
+print("~Under The Hood~")
+      
+print()
+a = "diesel"
+b = "gasoline"
+
+print("5. What engine would you like? ")
+answer5 = input("Please Enter 'diesel' or 'gasoline': ")    
+print(answer5)
+
+print("6.Would you like a summery of your choices?")
+answer6 = input("Enter 'yes'or 'no': ")
+print(answer6)
+
+# summery statement
+
+print("~Summary~")
+#how do I get it to print if than yes statements? 
+
+
+
